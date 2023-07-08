e09773d4dd4b6daf6c4c20cfd58d4fc269811efa
Wayne <waynelin30078@gmail.com>
2023-07-08 09:53:51 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/linwayne/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/linwayne/springbootmall/service/impl/ProductServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductDao productDao;
+
     @Override
     public Product getProductById(Integer productId) {
         return productDao.getProductById(productId);
@@ -19,5 +20,10 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Integer createProduct(ProductRequest productRequest) {
         return productDao.createProduct(productRequest);
+    }
+
+    @Override
+    public void updateProduct(Integer productId, ProductRequest productRequest) {
+        productDao.updateProduct(productId,productRequest);
     }
 }
